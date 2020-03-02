e3446ce2107bf3b4b45fb9b2c82ab9ed05f12362
Dima <d_osincev@mail.ru>
2020-03-02 22:31:36 +0600

    TestFixes

--- client/src/app/pages/create/CreatePage.tsx
+++ client/src/app/pages/create/CreatePage.tsx
@@ -22,18 +22,9 @@ const FormValues = {
 
 const CreatePage = () => {
 
-    console.log('RENDER')
-
-    let count = 0;
-    function Counter() {
-        count++;
-        // console.log(count)
-        if (count > 10) {
-            return;
-        }
-        Counter();
+    const test = () => {
+        console.log('test')
     }
-    Counter()
 
     const auth = useContext(AuthContext);
     const logout = () => {
